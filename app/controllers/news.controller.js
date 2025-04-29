0b700d72fec157e81d95b77f09e27b09e7e09ccc
Sara-Lib <jigoujitoku@gmail.com>
2025-04-29 13:48:35 +0100

    Completed task 3 with error handling

--- app/controllers/news.controller.js
+++ app/controllers/news.controller.js
@@ -1,5 +1,5 @@
 const endpoints = require('../../endpoints.json');
-const { selectTopics } = require('../../models/news.model');
+const { selectTopics, selectArticleById } = require('../../models/news.model');
 
 const getApiDocumentation = (req, res) => {
     res.status(200).send({ endpoints });
@@ -14,7 +14,21 @@ const getTopics = (req, res, next) => {
     //this is where we catch errors!
 };
 
+const getArticleById = (req, res, next) => {
+    const { article_id } = req.params;
+  
+    selectArticleById(article_id)
+      .then((article) => {
+        if (!article) {
+            return Promise.reject({ status: 404, msg: 'Article not found' });
+          }
+        res.status(200).send({ article });
+      })
+      .catch(next);
+  };
+
 module.exports = {
   getApiDocumentation,
-  getTopics
+  getTopics,
+  getArticleById
 };
--- models/news.model.js
+++ models/news.model.js
@@ -7,5 +7,19 @@ const selectTopics = () => {
       })
   };
   //no need for catch (next ) here, its in the controller
+
+  const selectArticleById = (article_id) => {
+    if (isNaN(Number(article_id))) {
+      return Promise.reject({ status: 400, msg: 'Invalid article ID' });
+    }
   
-  module.exports = { selectTopics };
+    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+      .then(({ rows }) => {
+        if (!rows.length) {
+          return Promise.reject({ status: 404, msg: 'Article not found' });
+        }
+        return rows[0];
+      });
+  };
+  
+  module.exports = { selectTopics, selectArticleById };
