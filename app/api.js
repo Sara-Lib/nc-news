b0056584d35a55595adf869256ff2dd53d27ff1a
Sara-Lib <jigoujitoku@gmail.com>
2025-04-29 14:58:42 +0100

    Task 4

--- app/api.js
+++ app/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("../db/connection");
-const { getApiDocumentation, getTopics, getArticleById } = require('./controllers/news.controller');
+const { getApiDocumentation, getTopics, getArticleById, getArticles } = require('./controllers/news.controller');
 
 const app = express()
 app.use(express.json())
@@ -8,6 +8,7 @@ app.use(express.json())
 app.get('/api', getApiDocumentation);
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/articles', getArticles)
 
 app.use((err, req, res, next) => {
     if (err.status && err.msg) {
--- models/news.model.js
+++ models/news.model.js
@@ -21,5 +21,21 @@ const selectTopics = () => {
         return rows[0];
       });
   };
+
+  const selectAllArticles = () => {
+    const queryStr = `
+      SELECT articles.author, articles.title, articles.article_id, articles.topic,
+             articles.created_at, articles.votes, articles.article_img_url,
+             COUNT(comments.comment_id)::INT AS comment_count
+      FROM articles
+      LEFT JOIN comments ON comments.article_id = articles.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;
+    `;
+  //use ::INT to cast as int quickly like with CAST(), otherwise it will be a string
+    return db.query(queryStr).then(({ rows }) => {
+      return rows;
+    });
+  };
   
-  module.exports = { selectTopics, selectArticleById };
+  module.exports = { selectTopics, selectArticleById, selectAllArticles };
