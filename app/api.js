e0568ea29678114e719adfd9451d3d966efbaf5f
Sara-Lib <jigoujitoku@gmail.com>
2025-05-21 11:55:55 +0100

    Added a test to check if cors working

--- app/api.js
+++ app/api.js
@@ -2,13 +2,21 @@ const express = require("express");
 const db = require("../db/connection");
 const { getApiDocumentation, getTopics, getArticleById, getArticles, getCommentsByArticleId, postComment, patchArticleVotes,removeComment, getUsers } = require('./controllers/news.controller');
 
-const app = express()
-app.use(express.json())
+const app = express();
+const cors = require('cors');
+
+app.use((req, res, next) => {
+  console.log('TESTING IF CORS ACTIVE');
+  next();
+});
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/api', getApiDocumentation);
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById);
-app.get('/api/articles', getArticles)
+app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 app.post('/api/articles/:article_id/comments', postComment);
 app.patch('/api/articles/:article_id', patchArticleVotes);
