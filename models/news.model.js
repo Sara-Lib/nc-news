d8075d9da10136d7a08fe044a4e410678f67eeaf
Sara-Lib <jigoujitoku@gmail.com>
2025-05-22 12:44:54 +0100

    Fixed an SQL error in the model - topics were not fetched correctly

--- models/news.model.js
+++ models/news.model.js
@@ -1,167 +1,189 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
 const selectTopics = () => {
-    return db.query('SELECT slug, description FROM topics;')
-      .then((result) => {
-        return result.rows;
-      })
-  };
-  //no need for catch (next ) here, its in the controller
+  return db.query("SELECT slug, description FROM topics;").then((result) => {
+    return result.rows;
+  });
+};
 
-  const selectArticleById = (article_id) => {
-    if (isNaN(Number(article_id))) {
-      return Promise.reject({ status: 400, msg: 'Invalid article ID' });
-    }
-  
-    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
-      .then(({ rows }) => {
-        if (!rows.length) {
-          return Promise.reject({ status: 404, msg: 'Article not found' });
-        }
-        return rows[0];
-      });
-  };
+const selectArticleById = (article_id) => {
+  if (isNaN(Number(article_id))) {
+    return Promise.reject({ status: 400, msg: "Invalid article ID" });
+  }
 
-  const sortByOptions = ["author", "title", "article_id", "topic", "created_at", "votes", "comment_count"];
-  const orderOptions = ["ASC", "DESC"];
-
-  //you can set the default optipon in the () of the callback fn!!
-  //they are added if there are no arguments
-  const selectAllArticles = (topic = null, sort_by = "created_at", order = "DESC") => {
-
-    if (!sortByOptions.includes(sort_by)) {
-      return Promise.reject({status: 400, msg: "Invalid sort_by option"})
-    }
-    
-    if (!orderOptions.includes(order)) {
-      return Promise.reject({status: 400, msg: "Invalid order option"})
-    }
-
-    let queryStr = `
-      SELECT articles.author, articles.title, articles.article_id, articles.topic,
-             articles.created_at, articles.votes, articles.article_img_url,
-             COUNT(comments.comment_id)::INT AS comment_count
-      FROM articles
-      LEFT JOIN comments ON comments.article_id = articles.article_id
-      GROUP BY articles.article_id
-      `;
-      
-      if (topic) {
-        queryStr += `WHERE articles.topic = $1`
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
       }
-      
-      queryStr += 
-              ` ORDER BY ${sort_by} ${order.toUpperCase()};`
-
-  //use ::INT to cast as int quickly like with CAST(), otherwise it will be a string
-    return db.query(queryStr).then(({ rows }) => {
-      return rows;
+      return rows[0];
     });
-  };
+};
 
-  const selectCommentsByArticleId = (article_id) => {
-    if (isNaN(Number(article_id))) {
-        return Promise.reject({ status:400, msg:"Invalid article ID"})
-    }
-    
-    return db.query(`
-        SELECT comment_id, votes, created_at, author, body, article_id
-        FROM comments
-        WHERE article_id = $1
-        ORDER BY created_at DESC;
-        `,
-        [article_id]
-        
+const sortByOptions = [
+  "author",
+  "title",
+  "article_id",
+  "topic",
+  "created_at",
+  "votes",
+  "comment_count",
+];
+const orderOptions = ["ASC", "DESC"];
+
+const selectAllArticles = (topic = null, sort_by = "created_at", order = "DESC") => {
+  if (!sortByOptions.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "Invalid sort_by option" });
+  }
+
+  if (!orderOptions.includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid order option" });
+  }
+
+  let queryStr = `
+    SELECT articles.author, articles.title, articles.article_id, articles.topic,
+           articles.created_at, articles.votes, articles.article_img_url,
+           COUNT(comments.comment_id)::INT AS comment_count
+    FROM articles
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+  `;
+
+  const queryValues = [];
+
+  if (topic) {
+    queryStr += ` WHERE articles.topic = $1`;
+    queryValues.push(topic);
+  }
+
+  queryStr += `
+    GROUP BY articles.article_id
+    ORDER BY ${sort_by} ${order.toUpperCase()};
+  `;
+
+  return db.query(queryStr, queryValues).then(({ rows }) => {
+    return rows;
+  });
+};
+
+const selectCommentsByArticleId = (article_id) => {
+  if (isNaN(Number(article_id))) {
+    return Promise.reject({ status: 400, msg: "Invalid article ID" });
+  }
+
+  return db
+    .query(
+      `
+      SELECT comment_id, votes, created_at, author, body, article_id
+      FROM comments
+      WHERE article_id = $1
+      ORDER BY created_at DESC;
+      `,
+      [article_id]
     )
-    .then(({rows}) => rows);
+    .then(({ rows }) => rows);
 };
 
 const insertComment = (article_id, username, body) => {
-      if (isNaN(Number(article_id))) {
-          return Promise.reject({ status:400, msg:"Invalid article ID"})
+  if (isNaN(Number(article_id))) {
+    return Promise.reject({ status: 400, msg: "Invalid article ID" });
+  }
+  if (!body || !username) {
+    return Promise.reject({ status: 400, msg: "Missing required fields" });
+  }
+
+  return db
+    .query(
+      `
+      INSERT INTO comments (author, body, article_id)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+      `,
+      [username, body, article_id]
+    )
+    .then(({ rows }) => rows[0])
+    .catch((err) => {
+      if (err.code === "23503") {
+        return Promise.reject({ status: 404, msg: "Article not found" });
       }
-    if (!body || !username) {
-        return Promise.reject({ status: 400, msg: 'Missing required fields' });
-      }
-    
-      return db.query(`
-          INSERT INTO comments (author, body, article_id)
-          VALUES ($1, $2, $3)
-          RETURNING *;
-        `,
-          [username, body, article_id]
-        )
-        .then(({ rows }) => rows[0])
-        .catch((err) => {
-            if (err.code === '23503') {
-                return Promise.reject({ status: 404, msg: 'Article not found' });
-              }
-        })
-  };
+    });
+};
 
 const updateArticleVotesById = (article_id, inc_votes) => {
-         if (isNaN(Number(article_id))) {
-        return Promise.reject({ status: 400, msg: 'Invalid article ID' });
-         }
-         if (typeof inc_votes !== 'number') {
-        return Promise.reject({ status: 400, msg: 'Missing or invalid inc_votes' });
-         }
+  if (isNaN(Number(article_id))) {
+    return Promise.reject({ status: 400, msg: "Invalid article ID" });
+  }
+  if (typeof inc_votes !== "number") {
+    return Promise.reject({ status: 400, msg: "Missing or invalid inc_votes" });
+  }
 
-        return db.query(`
-            UPDATE articles
-            SET votes = votes + $1
-            WHERE article_id = $2
-            RETURNING *;
-        `,
-        [inc_votes, article_id]
-        )
-        .then(({ rows }) => {
-            if (rows.length === 0) {
-                return Promise.reject({ status: 404, msg: 'Article not found' });
-            }
-            return rows[0]
-        })
-        .catch((err) => {
-            if (err.code === '23503') {
-                return Promise.reject({ status: 404, msg: 'Article not found' });
-              }
-        })
-    };
-    
+  return db
+    .query(
+      `
+      UPDATE articles
+      SET votes = votes + $1
+      WHERE article_id = $2
+      RETURNING *;
+      `,
+      [inc_votes, article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      return rows[0];
+    })
+    .catch((err) => {
+      if (err.code === "23503") {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+    });
+};
+
 const deleteCommentById = (comment_id) => {
-        return db.query(`
-            DELETE FROM comments
-            WHERE comment_id = $1
-            RETURNING *;
-            `,
-            [comment_id]
-        )
-        .then(({rows}) => {
-            if (rows.length === 0) {
-                return Promise.reject({ status: 404, msg: 'Comment not found' });
-            }
-            return rows[0]; //I think I don't need it here?
-        })
-        .catch((err) => {
-            if (err.code === '22P02') {
-                return Promise.reject({ status: 400, msg: 'Invalid comment ID' });
-              }
-              return Promise.reject(err);
-        });
-  };
+  return db
+    .query(
+      `
+      DELETE FROM comments
+      WHERE comment_id = $1
+      RETURNING *;
+      `,
+      [comment_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Comment not found" });
+      }
+      return rows[0];
+    })
+    .catch((err) => {
+      if (err.code === "22P02") {
+        return Promise.reject({ status: 400, msg: "Invalid comment ID" });
+      }
+      return Promise.reject(err);
+    });
+};
 
-  const selectAllUsers = () => {
-    return db.query(`
-        SELECT * FROM users;
-        `)
-        .then(({ rows }) => {
-            if (rows.length === 0) {
-                return Promise.reject({ status: 404, msg: 'User not found' });
-            }
-            return rows;
-        })
-        .catch();
-  };
+const selectAllUsers = () => {
+  return db
+    .query(`
+      SELECT * FROM users;
+      `)
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "User not found" });
+      }
+      return rows;
+    });
+};
 
-  
-  module.exports = { selectTopics, selectArticleById, selectAllArticles, selectCommentsByArticleId, insertComment, updateArticleVotesById, deleteCommentById, selectAllUsers};
+module.exports = {
+  selectTopics,
+  selectArticleById,
+  selectAllArticles,
+  selectCommentsByArticleId,
+  insertComment,
+  updateArticleVotesById,
+  deleteCommentById,
+  selectAllUsers,
+};
